1c2a1c77be1666b18f1edf79fc7a5f499c6cc318
Sechsing <shengxin1122@gmail.com>
2026-06-13 19:40:57 +0800

    Added profile page tests

---
+++ src/pages/ProfilePage.ts
@@ -0,0 +1,38 @@
+import { Page, Locator } from '@playwright/test';
+import { Header } from '../components/Header';
+import { Footer } from '../components/Footer';
+
+export class ProfilePage {
+    readonly page: Page;
+    readonly header: Header;
+    readonly profilePicture: Locator;
+    readonly username: Locator;
+    readonly editProfileButton: Locator;
+    readonly myPostsTab: Locator;
+    readonly favoritedPostsTab: Locator;
+    readonly articleCards: Locator;
+    readonly footer: Footer;
+    readonly userEmail: string;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.userEmail = process.env.REALWORLD_EMAIL || "";
+
+        // Header and Footer Element Locators
+        this.header = new Header(page);
+        this.footer = new Footer(page);
+
+        // Main Content Locators
+        this.profilePicture = page.getByRole('img', { name: /profile picture/i });
+        this.username = page.getByRole('heading', { name: `${this.userEmail}` });
+        this.editProfileButton = page.getByRole('button', { name: /Edit Profile Settings/i });
+        this.myPostsTab = page.getByRole('link', { name: /my posts/i });
+        this.favoritedPostsTab = page.getByRole('link', { name: /favorited posts/i });
+        this.articleCards = page.locator('app-profile').locator('app-article-preview');
+    }
+
+    async goto() {
+        await this.page.goto(`/profile/${this.userEmail}`);
+    }   
+}
+    
---
+++ tests/profile-page.spec.ts
@@ -0,0 +1,42 @@
+import { expect, test } from '@playwright/test';
+import { ProfilePage } from '../src/pages/ProfilePage';
+import { testHeader, testFooter } from './shared/layout';
+
+test.describe('Profile Page Tests', () => {
+    let profilePage: ProfilePage;
+
+    test.beforeEach(async ({ page }) => {
+        profilePage = new ProfilePage(page);
+        await profilePage.goto();
+    });
+
+    testHeader(() => profilePage);
+    testFooter(() => profilePage);
+
+    test('should display user profile picture and username', async () => {
+        await expect(profilePage.profilePicture).toBeVisible();
+        await expect(profilePage.username).toBeVisible();
+    });
+
+    test('should have edit profile settings button enabled', async () => {
+        await expect(profilePage.editProfileButton).toBeEnabled();
+        await profilePage.editProfileButton.click();
+    });
+
+        test('should display the my posts tab and navigate to the my posts page', async ({ page }) => {
+        await expect(profilePage.myPostsTab).toBeVisible();
+        await profilePage.myPostsTab.click();
+
+        await expect(page).toHaveURL(`/${profilePage.userEmail}`);
+    });
+
+    test('should display the favorited posts tab and navigate to the favorited posts page', async ({ page }) => {
+        await expect(profilePage.favoritedPostsTab).toBeVisible();
+        await profilePage.favoritedPostsTab.click();
+
+        await expect(page).toHaveURL(`/${profilePage.userEmail}/favorites`);
+    });
+});
+
+
+    
